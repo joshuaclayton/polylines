1be4b6a0c35ec25f94566f07e27c3ec3a7848378
Mitch Oliver <mitch@roadtrippers.com>
2014-08-13 18:46:03 -0400

    Allow arbitrary precision polylines. Google uses 5 digits of precision, however Mapbox uses 6.

--- lib/polylines/encoder.rb
+++ lib/polylines/encoder.rb
@@ -1,13 +1,13 @@
 module Polylines
   class Encoder < Base
-    def self.encode_points(points)
-      points_with_deltas = transform_to_array_of_lat_lng_and_deltas(points)
-      points_with_deltas.map {|point| encode(point) }.join
+    def self.encode_points(points, precision = 1e5)
+      points_with_deltas = transform_to_array_of_lat_lng_and_deltas(points, precision)
+      points_with_deltas.map {|point| encode(point, precision) }.join
     end
 
-    def self.encode(number)
+    def self.encode(number, precision = 1e5)
       self.new(number).tap do |encoding|
-        encoding.step_2
+        encoding.step_2 precision
         encoding.step_3
         encoding.step_4
         encoding.step_5
--- spec/polylines/encoder_spec.rb
+++ spec/polylines/encoder_spec.rb
@@ -4,20 +4,37 @@ describe Polylines::Encoder, ".encode" do
   it "encodes a single point" do
     Polylines::Encoder.encode(-179.9832104).should == "`~oia@"
   end
+
+  it "encodes a single point with 1e6 precision" do
+    Polylines::Encoder.encode(-179.9832104, 1e6).should == "ruhhvI"
+  end
 end
 
 describe Polylines::Encoder, ".encode_points" do
-  let(:polyline) { "_p~iF~ps|U_ulLnnqC_mqNvxq`@" }
   let(:points)   { [[38.5, -120.2], [40.7, -120.95], [43.252, -126.453]] }
 
-  it "encodes points correctly" do
-    Polylines::Encoder.encode_points(points).should == polyline
+  context "with default precision" do
+    let(:polyline) { "_p~iF~ps|U_ulLnnqC_mqNvxq`@" }
+    it "encodes points correctly" do
+      Polylines::Encoder.encode_points(points).should == polyline
+    end
+  end
+
+  context "with 1e6 precsion" do
+    let(:polyline) { "_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI" }
+    it "encodes points correctly" do
+      Polylines::Encoder.encode_points(points, 1e6).should == polyline
+    end
   end
 end
 
 describe Polylines::Encoder, ".encode_points that are very close together" do
   it "encodes points correctly" do
     Polylines::Encoder.encode_points([[41.3522171071184, -86.0456299662023],[41.3522171071183, -86.0454368471533]]).should == "krk{FdxdlO?e@"
+  end
+
+  it "encodes points correctly with 1e6 precision" do
+    Polylines::Encoder.encode_points([[41.3522171071184, -86.0456299662023],[41.3522171071183, -86.0454368471533]], 1e6).should == "q`}zmAzzxbcD?aK"
   end
 end
 
