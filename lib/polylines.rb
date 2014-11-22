000774282d47ba9df23bc1c334c47f9c62f6ac92
Joshua Clayton <jclayton@thoughtbot.com>
2014-11-21 22:52:33 -0500

    Replace autoload with require
    Closes #4

--- lib/polylines.rb
+++ lib/polylines.rb
@@ -1,5 +1,6 @@
+require "polylines/base"
+require "polylines/encoder"
+require "polylines/decoder"
+
 module Polylines
-  autoload :Base,    "polylines/base"
-  autoload :Encoder, "polylines/encoder"
-  autoload :Decoder, "polylines/decoder"
 end
